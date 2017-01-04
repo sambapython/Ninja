723ebdaa1353720eae9487d5ce797325c8fc896a
sambapython@gmail.com <sambapython@gmail.com>
2017-01-04 09:42:03 +0530

    2016-JAN-4

--- app3/app.py
+++ app3/app.py
@@ -1,7 +1,8 @@
-from flask import Flask, render_template, url_for, redirect
+from flask import Flask, render_template, url_for, redirect, request
 import pandas as pd
 app=Flask(__name__)
 import pdb
+import os
 
 @app.route('/')
 def home():
@@ -24,7 +25,28 @@ def persons(name):
 
 			return render_template('persons_info.html',
 				person_info = person)
+def get_path_images():
+	static_images = url_for('static',filename="images")
+	static_folder  = static_images.split('/')[1]
+	images  = static_images.split('/')[2]
+	file_path = os.path.join(static_folder, images)
+	return file_path
 
+
+
+@app.route('/fileupload',methods=['GET','POST'])
+def fileupload():
+	images_path = get_path_images()
+	images_names = os.listdir(images_path)
+	if request.method == "POST":
+		img = request.files.get('image')
+		
+		file_path = os.path.join(images_path, img.filename)
+		img.save(file_path)	
+	file_paths = []
+	for  image in images_names:
+		file_paths.append(os.path.join(images_path,image))
+	return render_template('form.html', file_paths = file_paths)
 
 
 if __name__ == "__main__":
